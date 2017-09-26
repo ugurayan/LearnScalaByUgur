2cb8cf74ef29c98c8d18227d7cb86aa5c7fd1bff
Ugur Ayan <ugur.ayan@gmail.com>
2017-09-26 20:41:00 +0200

    tutorial04

--- src/com/ugurayan/scala/tutorial_04/tutorial04.scala
+++ src/com/ugurayan/scala/tutorial_04/tutorial04.scala
@@ -3,7 +3,7 @@ package com.ugurayan.scala.tutorial_04
 /**
   * Created by future on 9/20/17.
   */
-object tutorial_04 {
+object tutorial04 {
   println("\n>>>>> TUTORIAL 04 : Traits & Class Composition with Mixins-------------\n")
 
 }
