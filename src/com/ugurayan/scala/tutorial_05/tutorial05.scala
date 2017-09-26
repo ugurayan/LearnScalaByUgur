33c8f0b1a67ad2aa7860d0edcdae90173ca25642
Ugur Ayan <ugur.ayan@gmail.com>
2017-09-26 20:41:33 +0200

    tutorial05

--- src/com/ugurayan/scala/tutorial_05/tutorial05.scala
+++ src/com/ugurayan/scala/tutorial_05/tutorial05.scala
@@ -1,9 +1,6 @@
-package com.ugurayan.scala.tutorial_04
+package com.ugurayan.scala.tutorial_05
 
-/**
-  * Created by future on 9/20/17.
-  */
-object tutorial_04 {
-  println("\n>>>>> TUTORIAL 04 : Traits & Class Composition with Mixins-------------\n")
+object tutorial05 {
+  println("\n>>>>> TUTORIAL 05 : Higher-order Functions -------------\n")
 
 }
