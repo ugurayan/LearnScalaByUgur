93bf466f3dd6391dcc7251d63d50170b92809b16
Ugur Ayan <ugur.ayan@gmail.com>
2017-09-26 20:40:40 +0200

    tutorial03

--- src/com/ugurayan/scala/tutorial_03/tutorial03.scala
+++ src/com/ugurayan/scala/tutorial_03/tutorial03.scala
@@ -1,27 +1,6 @@
-package com.ugurayan.scala.tutorial_02
+package com.ugurayan.scala.tutorial_03
 
-/**
-  * Created by future on 9/20/17.
-  */
-object tutorial02 {
-
-  val x: Long = 987654321
-  val y: Float = x  // 9.8765434E8 (note that some precision is lost in this case)
-
-  val face: Char = '☺'
-  val number: Int = face  // 9786
-
-
-  val list: List[Any] = List(
-    "a string",
-    1453,   // an integer
-    'x',   // a character
-    true,   // a boolean value
-    () => "an anonymous function returning a string"
-  )
-
-  println("\n>>>>> TUTORIAL 02 : Variables/ Unified TYpes ----------------------------\n")
-
-  list.foreach(element => println(element))
+object tutorial03 {
+  println("\n>>>>> TUTORIAL 03 : Classes ----------------------------\n")
 
 }
